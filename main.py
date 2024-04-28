1d640741fc4e5c3537365c4204344c7bc6dbcba4
Andrii Zubchenko <152086790+dissom@users.noreply.github.com>
2024-04-28 22:20:22 +0300

    Solution

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ from rest_framework.parsers import JSONParser
 
 def serialize_car_object(car: Car) -> bytes:
     serializer = CarSerializer(car)
-    data = serializer.data
-    serializer = CarSerializer(data=data)
     serializer.is_valid(raise_exception=True)
     json_data = JSONRenderer().render(serializer.data)
     return json_data
