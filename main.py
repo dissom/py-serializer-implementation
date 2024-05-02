235ac872e351a571bea3130da52c916c2313d29b
Andrii Zubchenko <152086790+dissom@users.noreply.github.com>
2024-05-02 12:35:44 +0300

    Solution

--- main.py
+++ main.py
@@ -19,23 +19,4 @@ def deserialize_car_object(json: bytes) -> Car:
     serializer = CarSerializer(data=data)
     serializer.is_valid(raise_exception=True)
     serializer.save()
-    return serializer.data
-
-
-if __name__ == "__main__":
-    car_data = {
-        "manufacturer": "Audi",
-        "model": "A4", "horse_powers": 200,
-        "is_broken": True
-    }
-
-    print(car_data)
-
-    serialized_data = serialize_car_object(car_data)
-    print(serialized_data)
-
-    deserialized_car = deserialize_car_object(serialized_data)
-    print(deserialized_car)
-
-    car_to_delete = Car.objects.get(pk=deserialized_car["id"])
-    car_to_delete.delete()
+    return serializer.instance
